f63e27fb40d6a69404050d47a8a214511143f3fc
Márton Balogh Bodor <marton2@gmail.com>
2022-12-04 21:51:21 +0100

    DAY 04 ANS 02 TRY 01

--- solutions/day04.rb
+++ solutions/day04.rb
@@ -5,13 +5,17 @@ chunks = lines
            .split("\n")
            .map { |l| l.split(",") }
            .map { |s| { a: s[0].split("-"), b: s[1].split("-") } }
-           .map { |s| {
-             a: { l: s[:a][0].to_i, r: s[:a][1].to_i },
-             b: { l: s[:b][0].to_i, r: s[:b][1].to_i }
-           } }
+           .map { |s| { l: (s[:a][0].to_i..s[:a][1].to_i), r: (s[:b][0].to_i..s[:b][1].to_i) } }
 
+# Check if range A contains range B or range B contains range A
 answer1 = chunks
-            .select { |c| (c[:a][:l] <= c[:b][:l] && c[:a][:r] >= c[:b][:r]) || (c[:b][:l] <= c[:a][:l] && c[:b][:r] >= c[:a][:r]) }
+            .select { |c| (c[:l].first <= c[:r].first && c[:l].last >= c[:r].last) || (c[:r].first <= c[:l].first && c[:r].last >= c[:l].last) }
+            .count
+
+# Check if range A starts before range B ends or range A ends before range B starts
+answer2 = chunks
+            .select { |c| c[:l].first <= c[:r].last && c[:r].first <= c[:l].last }
             .count
 
 puts "Answer 1: #{answer1}"
+puts "Answer 2: #{answer2}"
