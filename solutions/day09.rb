b60530313751a4c35b1890090b0c4acc2caf88c3
Márton Balogh Bodor <marton2@gmail.com>
2022-12-09 16:49:50 +0100

    DAY 09 ANS 02 TRY 01

--- solutions/day09.rb
+++ solutions/day09.rb
@@ -1,38 +1,53 @@
 # frozen_string_literal: true
-require 'matrix'
 
-# Create a matrix from the input
+def simulate(lines, knots)
+  visits = []
+
+  lines.each do |s|
+    # From every line, take the direction and the number of steps
+    dir = s[:d]
+    cnt = s[:c]
+
+    (0..cnt - 1).each do |_|
+      knots.each_with_index do |_, index|
+        # Always move the head based on the direction
+        if index == 0 then
+          knots[index] = {
+            x: if dir == 'R' then knots[index][:x] + 1 elsif dir == 'L' then knots[index][:x] - 1 else knots[index][:x] end,
+            y: if dir == 'U' then knots[index][:y] - 1 elsif dir == 'D' then knots[index][:y] + 1 else knots[index][:y] end
+          }
+        else
+          # Calculate the previous knot's distance
+          xdist = (knots[index - 1][:x] - knots[index][:x])
+          ydist = (knots[index - 1][:y] - knots[index][:y])
+
+          # When one coordinate is further than 2 steps...
+          if xdist.abs >= 2 || ydist.abs >= 2 then
+            # Move the knot to the direction of the previous knot
+            # Move a maimmum of 1 step into the direction (so half if the distance is 2, 1 or 0 otherwise)
+            knots[index] = {
+              x: xdist.abs == 2 ? knots[index][:x] + xdist / 2 : knots[index][:x] + xdist,
+              y: ydist.abs == 2 ? knots[index][:y] + ydist / 2 : knots[index][:y] + ydist
+            }
+          end
+        end
+      end
+
+      visits |= [knots.last]
+    end
+  end
+
+  visits
+end
+
 lines = File
           .read('../inputs/day09.txt')
           .split("\n")
           .map { |l| l.split(" ") }
           .map { |l| {d: l[0], c: l[1].to_i}}
 
-visits = []
-head = { x: 0, y: 0 }
-memo = { x: 0, y: 0 }
-tail = { x: 0, y: 0 }
+visits1 = simulate(lines, Array.new(2, {x: 0, y: 0}))
+visits2 = simulate(lines, Array.new(10, { x: 0, y: 0 }))
 
-lines.each do |s|
-  dir = s[:d]
-  cnt = s[:c]
-
-  (0..cnt - 1).each do |_|
-    head = {
-      x: if dir == 'R' then head[:x] + 1 elsif dir == 'L' then head[:x] - 1 else head[:x] end,
-      y: if dir == 'U' then head[:y] - 1 elsif dir == 'D' then head[:y] + 1 else head[:y] end
-    }
-
-    xdist = (head[:x] - tail[:x]).abs
-    ydist = (head[:y] - tail[:y]).abs
-
-    tail = memo if xdist >= 2 || ydist >= 2
-    visits |= [tail]
-
-    memo = head
-  end
-end
-
-answer1 = visits.count
-
-puts "Answer 1: #{answer1}"
+puts "Answer 1: #{visits1.count}"
+puts "Answer 2: #{visits2.count}"
