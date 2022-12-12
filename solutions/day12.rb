8b91be7c682d2bf54dd02b21d363eedf465087ea
Márton Balogh Bodor <marton2@gmail.com>
2022-12-12 22:31:57 +0100

    DAY 12 ANS 01 TRY 02

--- solutions/day12.rb
+++ solutions/day12.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 require 'matrix'
 
-def should_visit(map, x, y, d)
+def should_visit(map, x, y, d, w)
   return false if x < 0 || y < 0
   return false if map[y].nil? || map[y][x].nil?
-  s = map[y][x][:d]
-  d < s
+  item = map[y][x]
+  level = if item[:l] != -28 then item[:l] else 26 end
+  distance = item[:d]
+  d < distance && level <= w
 end
 
 def update_distances(map, cursor)
@@ -14,51 +16,39 @@ def update_distances(map, cursor)
   d = cursor[:d]
   w = cursor[:w]
 
-  return if x < 0 || y < 0
-  return if map[y].nil? || map[y][x].nil?
+  left = map[y][x - 1] if should_visit(map, x - 1, y, d + 1, w)
+  right = map[y][x + 1] if should_visit(map, x + 1, y, d + 1, w)
+  top = map[y - 1][x] if should_visit(map, x, y - 1, d + 1, w)
+  bottom = map[y + 1][x] if should_visit(map, x, y + 1, d + 1, w)
 
-  item = map[y][x]
-  level = if item[:l] != -28 then item[:l] else 26 end
-  return if level > w
+  left[:d] = d + 1 unless left.nil?
+  right[:d] = d + 1 unless right.nil?
+  top[:d] = d + 1 unless top.nil?
+  bottom[:d] = d + 1 unless bottom.nil?
 
-  item[:d] = [d, item[:d]].min
-
-  update_distances(map, { x: x + 1, y: y, d: d + 1, w: w }) if should_visit(map, x + 1, y, d + 1)
-  update_distances(map, { x: x, y: y + 1, d: d + 1, w: w }) if should_visit(map, x, y + 1, d + 1)
-  update_distances(map, { x: x - 1, y: y, d: d + 1, w: w }) if should_visit(map, x - 1, y, d + 1)
-  update_distances(map, { x: x, y: y - 1, d: d + 1, w: w }) if should_visit(map, x, y - 1, d + 1)
+  [left, right, top, bottom].filter { |t| !t.nil? }
 end
 
-def flood_route(map)
-  while map.flatten.any? { |t| t[:d] == 9999 && t[:l] == -28 } do
-    candidates = []
-    map.each_with_index do |r, row|
-      r.each_with_index do |c, col|
-        next if c[:d] == 9999
-        candidates.append({ x: col, y: row, t: c })
-      end
-    end
-
-    candidates.each do |c|
-      tile = c[:t]
-      wall = if tile[:l] == -28 then 26 elsif tile[:l] == -14 then 0 else tile[:l] + 1 end
-      dstn = tile[:d]
-
-      cursor = { x: c[:x], y: c[:y], d: dstn, w: wall }
-      update_distances(map, cursor)
-    end
+def flood_route(map, tile)
+  changes = []
+  while !tile.nil? do
+    wall = if tile[:l] == -28 then 26 elsif tile[:l] == -14 then 0 else tile[:l] + 1 end
+    cursor = { x: tile[:x], y: tile[:y], d: tile[:d], w: wall }
+    changes +=  update_distances(map, cursor)
+    tile = changes.shift
   end
 end
 
 map = File.read('../inputs/day12.txt')
           .split("\n")
-          .map { |l| l.split("") }
-          .map { |l| l.map { |c| { l: c.ord - 97, d: 9999, v: false } } }
+          .each_with_index
+          .map { |l, r| [r, l.split("")] }
+          .map { |r, l| l.each_with_index.map { |t, c| { x: c, y: r, l: t.ord - 97, d: 9999, v: false } } }
 
 start_y, start_x = map.flatten.index { |t| t[:l] == -14 }.divmod(map[0].length)
 end_y, end_x = map.flatten.index { |t| t[:l] == -28 }.divmod(map[0].length)
 
 map[start_y][start_x][:d] = 0
-flood_route(map)
+flood_route(map, map[start_y][start_x])
 
 puts "Answer 1: #{map[end_y][end_x][:d]}"
