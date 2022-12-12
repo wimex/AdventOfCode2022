35147471dd605f42ce54c608fa8917bab5473733
Márton Balogh Bodor <marton2@gmail.com>
2022-12-12 23:03:26 +0100

    DAY 12 ANS 02 TRY 01

--- solutions/day12.rb
+++ solutions/day12.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 require 'matrix'
 
+# Check if a tile should be visited
+#  - it should be inside the bounds of the array
+#  - it should have a level that's not too high
+#  - it should be on a shorter route
 def should_visit(map, x, y, d, w)
   return false if x < 0 || y < 0
   return false if map[y].nil? || map[y][x].nil?
@@ -10,6 +14,7 @@ def should_visit(map, x, y, d, w)
   d < distance && level <= w
 end
 
+# Update the distances around the current tile
 def update_distances(map, cursor)
   x = cursor[:x]
   y = cursor[:y]
@@ -29,12 +34,16 @@ def update_distances(map, cursor)
   [left, right, top, bottom].filter { |t| !t.nil? }
 end
 
+# Instead of proper trace routing (slow) just flood until we run into a wall
+# While there are changed tiles, update the distances around the current tile
 def flood_route(map, tile)
   changes = []
+
   while !tile.nil? do
     wall = if tile[:l] == -28 then 26 elsif tile[:l] == -14 then 0 else tile[:l] + 1 end
     cursor = { x: tile[:x], y: tile[:y], d: tile[:d], w: wall }
-    changes +=  update_distances(map, cursor)
+
+    changes += update_distances(map, cursor)
     tile = changes.shift
   end
 end
@@ -45,10 +54,19 @@ map = File.read('../inputs/day12.txt')
           .map { |l, r| [r, l.split("")] }
           .map { |r, l| l.each_with_index.map { |t, c| { x: c, y: r, l: t.ord - 97, d: 9999, v: false } } }
 
-start_y, start_x = map.flatten.index { |t| t[:l] == -14 }.divmod(map[0].length)
+starts = map.flatten.filter { |t| t[:l] == -14 || t[:l] == 0 }
 end_y, end_x = map.flatten.index { |t| t[:l] == -28 }.divmod(map[0].length)
+results = []
 
-map[start_y][start_x][:d] = 0
-flood_route(map, map[start_y][start_x])
+while !starts.empty?
+  map.flatten.each { |m| m[:d] = 9999 } # Reset distances - let's just use an arbitrary value instead of INT_MAX
 
-puts "Answer 1: #{map[end_y][end_x][:d]}"
+  current = starts.shift
+  current[:d] = 0
+
+  flood_route(map, current)
+  results.push({ x: current[:x], y: current[:y], l: current[:l], d: map[end_y][end_x][:d] })
+end
+
+puts "Answer 1: #{results.filter { |r| r[:l] == -14 }.first[:d]}"
+puts "Answer 2: #{results.map { |r| r[:d] }.min}"
